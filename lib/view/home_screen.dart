3b8ef83c1fe794babb7fceb760ba450e30d2a5e4
sprupak07 <sapkotarupak343@gmail.com>
2022-12-18 12:25:11 +0545

    home screen created

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Column(
+        children: const [
+          Text('Home Screen'),
+        ],
+      ),
+    );
+  }
+}

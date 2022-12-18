07645e9f1fb4c8f3ee05a220b640eb1cf4db8255
sprupak07 <sapkotarupak343@gmail.com>
2022-12-18 15:15:04 +0545

    colors added

---
+++ lib/res/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF1E88E5);
+  static const Color white = Colors.white;
+  static const Color primaryColorDark = Color(0xFF1565C0);
+  static const Color primaryColorLight = Color(0xFF64B5F6);
+  static const Color accentColor = Color(0xFF00ACC1);
+  static const Color primaryTextColor = Color(0xFF212121);
+  static const Color secondaryTextColor = Color(0xFF757575);
+  static const Color dividerColor = Color(0xFFBDBDBD);
+}

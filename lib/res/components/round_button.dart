5c920db94eb70714480b8077161771b2463d8ed6
sprupak07 <sapkotarupak343@gmail.com>
2022-12-30 19:37:30 +0545

    button created

--- lib/res/components/round_button.dart
+++ lib/res/components/round_button.dart
@@ -4,31 +4,42 @@ import 'package:mvvm/res/colors.dart';
 class RoundButton extends StatelessWidget {
   final String title;
   final bool loading;
+  final double height;
+  final double width;
   final onPressed;
 
   const RoundButton(
-      {super.key, required this.title, this.onPressed, this.loading = false});
+      {super.key,
+      required this.title,
+      this.onPressed,
+      this.loading = false,
+      required this.height,
+      required this.width});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onPressed,
       child: Container(
-        height: 20,
-        width: 20,
+        height: height,
+        width: width,
         decoration: BoxDecoration(
           color: AppColors.primaryColor,
           borderRadius: BorderRadius.circular(10),
         ),
         child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(
-              color: AppColors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: loading
+              ? CircularProgressIndicator(
+                  color: Colors.white,
+                )
+              : Text(
+                  title,
+                  style: const TextStyle(
+                    color: AppColors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );

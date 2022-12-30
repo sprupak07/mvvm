687f39aba8b8ed98a2e0bfc75c818917f494f362
sprupak07 <sapkotarupak343@gmail.com>
2022-12-30 19:38:14 +0545

    app url added

---
+++ lib/res/app_url.dart
@@ -0,0 +1,5 @@
+class AppUrl {
+  static const String baseUrl = 'https://reqres.in/';
+  static const String loginUrl = '${baseUrl}api/login';
+  static const String registerUrl = '${baseUrl}api/register';
+}
